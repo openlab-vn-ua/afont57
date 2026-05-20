a2138beb6b8b7ede8d6d43765a21203cf1ac5767
Serge Ageyev <justboss777@gmail.com>
2026-05-20 16:50:22 +0300

    Refined implementation

--- afont572bincpp.py
+++ afont572bincpp.py
@@ -1,64 +1,38 @@
+#!/usr/bin/env python3
+
 #Python 3.7.8+
 import re
 import sys
 
-MSG_ERR = 'ERROR:'
+# Default fixed font:
+# Declared 5x7
+# Actual font cell is 5x8, software uses 6x8 print cells (that is, adds 1px margin to right of each char)
+# So, vertical pseudograph is linked, horizontal is not (that is, chars are separated by 1px enforced gap)
+# Font char stored 5 bytes, matrix 8x5, rotated 90 deg CW
+# Dumping chars as binary provides much more visibility of actual char pixel pattern
+
+MSG_WARN = 'WARN:'
+MSG_FAIL = 'FAIL:'
 
 EXCODE_FAIL = 1
 EXCODE_OK   = 0
 
-def proccessArgs5(args):
-  INDENT = "    "
-
-  pattern = re.compile(r"^[ \t]*((?:0x)?[0-9a-f]{1,2})[ \t]*,[ \t]*((?:0x)?[0-9a-f]{1,2})[ \t]*,[ \t]*((?:0x)?[0-9a-f]{1,2})[ \t]*,[ \t]*((?:0x)?[0-9a-f]{1,2})[ \t]*,[ \t]*((?:0x)?[0-9a-f]{1,2})[ \t]*(,|)[ \t]*(.*|)$", flags=re.IGNORECASE)
-
-  with open(args.fontin, "rt", encoding="utf-8") as f:
-    linesin = [linein.rstrip("\n\r") for linein in f]
-
-  ci = 0
-  linesout = []
-  for linein in linesin:
-    m = pattern.match(linein)
-    if m:
-      ###lineout = m.group(1)+", "+m.group(2)+", "+m.group(3)+", "+m.group(4)+", "+m.group(5)+m.group(6)
-      values = [int(m.group(i), 0) for i in range(1,6)]
-
-      lineout = ""
-      lineout += "\n"
-      lineout += INDENT + "// 0x{:02x} {:3d}".format(ci, ci)
-      if (ci >= 0x20) and (ci < 127):
-        lineout += " '{}'".format(chr(ci))
-      else:
-        if (m.group(7) != ""):
-          lineout += "    "
-
-      if (m.group(7) != ""):
-        lineout += " " + m.group(7)
-
-      lineout += "\n"
-      lineout += ",\n".join(INDENT + "0b{:08b}".format(v) for v in values)+m.group(6)
-      linesout.append(lineout)
-      ci += 1
-    else:
-      linesout.append(linein)
-
-  with open(args.fontout, "wt", encoding="utf-8") as f:
-    for lineout in linesout:
-      f.write(lineout+'\n')      
-
-  return EXCODE_OK
-
-def proccessArgs(args):
+def processArgs(args):
   INDENT = "    "
 
   head = re.compile(r".*\[\s*\].*{\s*", flags=re.IGNORECASE)
-  pattern = re.compile(r"((?:0x|0b)?[0-9a-f]{1,8})[ \t]*(,|\)[ \t]*,|)[ \t]*(//.*$|)", flags=re.IGNORECASE)
+
+  # We do not support all the cases in regexp
+  # some errors catched in int values parsing (special case is absense of last comma)
+  # so we have adequete balance here
+  pattern = re.compile(r"(0x[0-9a-f]+|0b[0-9a-f]+|0|[1-9][0-9]*)[ \t]*(,|\)[ \t]*,|)[ \t]*(//.*$|)", flags=re.IGNORECASE)
+
   tail = re.compile(r"\s*}\s*;", flags=re.IGNORECASE)
 
   with open(args.fontin, "rt", encoding="utf-8") as f:
     linesin = [linein.rstrip("\n\r") for linein in f]
 
-  FONT_IN_BYTES_PER_CHAR = 5
+  FONT_IN_BYTES_PER_CHAR = 5 # actual char matrix 5 bytes
 
   byteIndex = 0
   charIndex = 0
@@ -67,7 +41,10 @@ def proccessArgs(args):
   head_found = False
   tail_found = False
 
+  lineNumber = 0
+
   for linein in linesin:
+    lineNumber += 1
     if not head_found:
       if head.match(linein):
         head_found = True
@@ -113,7 +90,7 @@ def proccessArgs(args):
         value = None
       
       if (value is None) or (value < 0) or (value > 255):
-        #print(MSG_ERR+"Invalid value in line: "+linein+":"+m.group(1))
+        print(MSG_WARN+f"Invalid value in line {lineNumber}: '{m.group(1)}' -- ignored")
         continue # skip invalid lines
 
       sep     = m.group(2)
@@ -148,16 +125,24 @@ def proccessArgs(args):
       if (byteIndex % FONT_IN_BYTES_PER_CHAR == 0):
         charIndex += 1
 
-  with open(args.fontout, "wt", encoding="utf-8") as f:
-    for lineout in linesout:
-      f.write(lineout+'\n')
+  if not head_found:
+      print(MSG_FAIL + "Font array head not found")
+      return EXCODE_FAIL
 
-  byteCount = byteIndex
+  if not tail_found:
+      print(MSG_FAIL + "Font array tail not found")
+      return EXCODE_FAIL
+
   charCount = 256
+  byteCount = byteIndex
   goodCount = charCount * FONT_IN_BYTES_PER_CHAR
 
   if (byteCount != goodCount):
-    print(f"WARN: font size {byteCount} bytes does not match expected size of {goodCount} bytes")
+    print(MSG_WARN+f"font size {byteCount} bytes does not match expected size of {goodCount} bytes")
+
+  with open(args.fontout, "wt", encoding="utf-8") as f:
+    for lineout in linesout:
+      f.write(lineout+'\n')
 
   print(f"Converted {byteCount} bytes")
 
@@ -167,21 +152,22 @@ def main(argv):
 
   import argparse
 
-  parser = argparse.ArgumentParser(description='Adafruit gfx FONT 5x7 definition converter to 0bxxxxxxxx')
+  parser = argparse.ArgumentParser(description='Adafruit fixed font 5x7 definition converter to 0bxxxxxxxx')
   parser.add_argument('fontin', type=str, help='Input 5x7 font file (.h or .cpp)')
   parser.add_argument('fontout', type=str, help='Output 5x7 font file (.h or .cpp)')
   parser.add_argument('--nocomcode', help='Do not add char code comments', action='store_true')
+  parser.add_argument('--debug', help='Print extra info on exceptions', action='store_true')
   args = parser.parse_args(argv)
 
   try:
-    if proccessArgs(args) != 0:
+    if processArgs(args) != 0:
       return(EXCODE_FAIL)
     print("OK!")
   except Exception as e:
     if hasattr(e,'reason'): print("FAIL "+e.reason); 
     else: print("FAIL:"+str(e))
-    raise e # uncomment this like to have traceback printed
-    print(MSG_ERR+"Error while in operation")
+    if (args.debug): raise e
+    print(MSG_FAIL+"Error while in operation")
     return(EXCODE_FAIL)
   
   return(EXCODE_OK)
